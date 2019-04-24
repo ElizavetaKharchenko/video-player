37a4da2e2a3946b82d08ae19048d74c7ddad2b96
anka1928 <lizaveta_r@list.ru>
2019-04-24 12:42:56 +0300

    add overlay reload button

--- src/app.js
+++ src/app.js
@@ -7,27 +7,41 @@ export default () => {
   const video = document.getElementById('mainVideo');
   const playPause = document.getElementById('playPause');
   const dynamic = document.getElementById('dynamic');
-  const overlayBtn = document.getElementById('overlay-btn-play');
+  const overlayPlayBtn = document.getElementById('overlay-btn-play');
+  const overlayReloadBtn = document.getElementById('overlay-btn-reload');
 
   const togglePlayPause = () => video.paused ? video.play() : video.pause();
 
   video.onplay = () => {
     playPause.classList.replace('fa-play-circle', 'fa-stop')
-    overlayBtn.style.visibility = 'hidden';
+    overlayPlayBtn.style.visibility = 'hidden';
   };
   video.onpause = () => {
     playPause.classList.replace('fa-stop', 'fa-play-circle');
-    overlayBtn.style.visibility = 'visible';
+    overlayPlayBtn.style.visibility = 'visible';
   };
   video.onclick = () => togglePlayPause();
+  video.onended = () => {
+    overlayReloadBtn.style.visibility = 'visible';
+    overlayPlayBtn.style.visibility = 'hidden';
+  }
+
+  overlayReloadBtn.style.visibility = 'hidden';
+
   
   playPause.addEventListener('click', () => {
     togglePlayPause();
   });
 
-  overlayBtn.addEventListener('click', () => {
+  overlayPlayBtn.addEventListener('click', () => {
     video.play();
   });
+
+  overlayReloadBtn.addEventListener('click', () => {
+    video.load();
+    video.play();
+    overlayReloadBtn.style.visibility = 'hidden';
+  })
 
   dynamic.addEventListener('click', () => {
     video.muted = !video.muted;
