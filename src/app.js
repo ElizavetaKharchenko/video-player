d2dcef76c5d4ab83b223e47164738b2475db8547
anka1928 <lizaveta_r@list.ru>
2019-04-24 11:42:56 +0300

    add basic logic

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
-import WatchJS from 'melanke-watchjs';
+// import WatchJS from 'melanke-watchjs';
 import "@babel/polyfill";
 import formattedTime from './utils';
 
 export default () => {
-  const state = {
-    playpause: true,
-    mute: true,
-  };
-
-  const { watch } = WatchJS;
-  const video = document.getElementById('mainVideo');
-
   
+  const video = document.getElementById('mainVideo');
+  const playPause = document.getElementById('playPause');
+  const dynamic = document.getElementById('dynamic');
 
+  const togglePlayPause = () => video.paused ? video.play() : video.pause();
+
+  video.onplay = () => playPause.classList.replace('fa-play-circle', 'fa-stop');
+  video.onpause = () => playPause.classList.replace('fa-stop', 'fa-play-circle');
+  video.onclick = () => togglePlayPause();
+  
+  playPause.addEventListener('click', () => {
+    togglePlayPause();
+  });
+
+  dynamic.addEventListener('click', () => {
+    video.muted = !video.muted;
+    dynamic.classList.contains('fa-volume-off') ? dynamic.classList.replace('fa-volume-off', 'fa-volume-up') :
+      dynamic.classList.replace('fa-volume-up', 'fa-volume-off');
+  });
+  
+  
 
 };
