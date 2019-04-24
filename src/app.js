452d57c7e3d0e1e8866a6d97ae7b8b889c637629
anka1928 <lizaveta_r@list.ru>
2019-04-24 12:07:09 +0300

    add overlay button

--- src/app.js
+++ src/app.js
@@ -3,19 +3,30 @@ import "@babel/polyfill";
 import formattedTime from './utils';
 
 export default () => {
-  
+
   const video = document.getElementById('mainVideo');
   const playPause = document.getElementById('playPause');
   const dynamic = document.getElementById('dynamic');
+  const overlayBtn = document.getElementById('overlay-btn-play');
 
   const togglePlayPause = () => video.paused ? video.play() : video.pause();
 
-  video.onplay = () => playPause.classList.replace('fa-play-circle', 'fa-stop');
-  video.onpause = () => playPause.classList.replace('fa-stop', 'fa-play-circle');
+  video.onplay = () => {
+    playPause.classList.replace('fa-play-circle', 'fa-stop')
+    overlayBtn.style.visibility = 'hidden';
+  };
+  video.onpause = () => {
+    playPause.classList.replace('fa-stop', 'fa-play-circle');
+    overlayBtn.style.visibility = 'visible';
+  };
   video.onclick = () => togglePlayPause();
   
   playPause.addEventListener('click', () => {
     togglePlayPause();
+  });
+
+  overlayBtn.addEventListener('click', () => {
+    video.play();
   });
 
   dynamic.addEventListener('click', () => {
